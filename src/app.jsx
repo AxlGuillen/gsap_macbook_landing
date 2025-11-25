6a00c6e56cf7cbdc22c5118ed8b0cc104f48c41c
Axl Eduardo Guillen Luna <axl.guillen@urvenue.com>
2025-11-25 16:48:18 -0600

    Add ProductViewer and StudioLights components

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
+import ProductViewer from './components/ProductViewer.jsx';
 
 const App = () => {
     return (
       <main>
         <Navbar />
         <Hero />
+        <ProductViewer />
       </main>
     );
 }
