2650576ac499477409248425f02668adad90a8a5
Axl Eduardo Guillen Luna <axl.guillen@urvenue.com>
2025-11-25 13:25:55 -0600

    Add Hero component and integrate into App
    Created a new Hero component displaying product information and a video, and added it to the main App component below the Navbar.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,11 @@
 import Navbar from './components/navbar.jsx';
+import Hero from './components/hero.jsx';
 
 const App = () => {
     return (
       <main>
         <Navbar />
-
+        <Hero />
       </main>
     );
 }
