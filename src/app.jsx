cac688192f30c7ad1eb7c7892b7b8946fadd8d03
Axl Eduardo Guillen Luna <132869278+AxlGuillen@users.noreply.github.com>
2025-11-30 19:50:18 -0600

    Add Showcase component and integrate into App

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
 import ProductViewer from './components/ProductViewer.jsx';
+import Showcase from './components/Showcase.jsx';
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 
@@ -12,6 +13,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <ProductViewer />
+        <Showcase />
       </main>
     );
 }
